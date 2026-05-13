7c5e3851faeb252c9943d4a8a81fa5a1139c0bd3
sala7 <kssoto29@ucatolica.edu.co>
2026-05-13 13:44:50 -0500

    ip

--- src/main/java/com/example/proyecto/TestMongoConnection.java
+++ src/main/java/com/example/proyecto/TestMongoConnection.java
@@ -4,7 +4,7 @@ import com.example.proyecto.service.MongoDBService;
 
 public class TestMongoConnection {
     public static void main(String[] args) {
-        System.out.println("Probando conexión a MongoDB en 172.30.16.165:27017");
+        System.out.println("Probando conexión a MongoDB en 172.30.16.104:27017");
 
         // Intentar conectar
         MongoDBService.conectar();
